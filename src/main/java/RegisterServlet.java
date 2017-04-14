ea25b7c250595242bf18b24b3a892f7c09e986f4
imthapa <ishwarmani.thapa@tothenew.com>
2017-04-14 15:28:29 +0530

    modifications

--- src/main/java/RegisterServlet.java
+++ src/main/java/RegisterServlet.java
@@ -14,7 +14,7 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("WEB-INF/register.jsp").forward(req,resp);
+        req.getRequestDispatcher("WEB-INF/register.jsp").forward(req, resp);
     }
 
     @Override
